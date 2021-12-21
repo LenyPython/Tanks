7b48e51264cb40d5fe913f46b4369c1974a77763
LenyPython <buniadyserv@gmail.com>
2021-12-21 19:43:13 +0100

    Rebuild sprites now they change diractrion when they move

--- src/Tanks/Tank.ts
+++ src/Tanks/Tank.ts
@@ -1,3 +1,4 @@
+import {DIRECTION} from '.'
 import Sprite from '../Sprite'
 
 const enemySprite = require('../imgs/Enemy.png')
@@ -19,11 +20,25 @@ export default class Tank {
 	drawSprite(){
 		if(this.sprite.isMoving) this.sprite.frame++
 		if(this.sprite.frame > this.maxFrame) this.sprite.frame = 0
+		let rotation = 0
+		switch(this.sprite.direction){
+			case DIRECTION.DOWN:
+				rotation = 1 * this.sprite.size
+				break
+			case DIRECTION.RIGHT:
+				rotation = 2 * this.sprite.size
+				break
+			case DIRECTION.LEFT:
+				rotation = 3 * this.sprite.size
+				break
+
+		}
 
 		this.ctx.drawImage(
 			this.sprite.img, 
 			//how to crop image x px on each frame, y px always 0
-			this.sprite.frame * this.sprite.size, 0,
+			this.sprite.frame * this.sprite.size,
+			rotation,
 			//size of croped img
 			this.sprite.size,
 			this.sprite.size,
@@ -34,7 +49,6 @@ export default class Tank {
 			this.sprite.size,
 			this.sprite.size,
 			)
-
 	}
 
 
