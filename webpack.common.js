7ffb94abbd1ad3b1670f9cf8d8103d862afdc4cb
LenyPython <buniadyserv@gmail.com>
2021-12-25 19:28:49 +0100

    Adding HtmlPlugin to webpack and addin html template

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,14 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.ts',
 	devtool: 'inline-source-map',
+	entry: {
+		main: path.resolve(__dirname, 'src/index.ts')
+	},
 	output: {
-		filename: 'script.js',
-		path: path.resolve(__dirname, 'public'),
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
 	module: {
 		rules: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const common = require('./webpack.common')
 const webpackMerge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = webpackMerge.merge(common, {
 	mode: "production",
-	entry: './src/index.ts',
-	output: {
-		filename: 'script.js',
-		path: path.resolve(__dirname, 'public'),
+	entry: {
+		main: path.resolve(__dirname, 'src/index.ts')
 	},
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [new HtmlWebpackPlugin({
+		title: "Tanks Pew Pew",
+		filename: 'index.html',
+		template: path.resolve(__dirname, 'src/template.html')
+	}
+
+	)]
 });
